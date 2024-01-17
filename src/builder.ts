8aba213afa10c92507cc615136711f13912165e6
Guihgo <guihgo100milha@gmail.com>
2024-01-17 18:10:09 -0300

    [feature] build as development mode

--- src/builder.ts
+++ src/builder.ts
@@ -13,11 +13,18 @@ import { log, success, tab } from "./tools/console_io.js"
 const __dirname = dirname(fileURLToPath(import.meta.url))
 const __filename = fileURLToPath(import.meta.url)
 
+enum Enviroment {
+    Development = "development",
+    Production = "production"
+}
+
 async function build(coreFileName: string) {
+
+    const env = (process.argv[2]) ? (process.argv[2].toLowerCase() as Enviroment) : Enviroment.Production
 
     const packageJson = JSON.parse(readFileSync(resolve(__dirname, "../package.json"), { encoding: "utf-8" }))
 
-    log(`─── Building ${packageJson.name}@v${packageJson.version}`)
+    log(`───  [${env}] Building ${packageJson.name}@v${packageJson.version}`)
 
     /* Copy package.json to dist dir */
     writeFileSync(resolve(__dirname, "package.json"), JSON.stringify(packageJson, undefined, tab))
@@ -33,22 +40,31 @@ async function build(coreFileName: string) {
     }, [/\.js/])
 
     /* Build AtomicReact Core Minified */
-    const atomicreactMinifiedPath = resolve(__dirname, coreFileName);
+    const atomicreactMinifiedPath = resolve(__dirname, coreFileName)
 
     const loaderDOMHelper = readFileSync(resolve(domHelpersPathTo, "loader.js"), { encoding: "utf-8" })
     const lib = transpileModule(readFileSync(resolve(__dirname, `lib.js`), { encoding: "utf-8" }), getTranspileOptions("atomicreact")).outputText
 
-    const minifiedJS = await minify({
-        "loader.js": loaderDOMHelper,
-        "lib.js": lib
-    }, {
-        toplevel: true,
-        keep_classnames: true,
-        keep_fnames: false,
-        compress: true
-    })
+    let outJS = ""
 
-    writeFileSync(atomicreactMinifiedPath, minifiedJS.code, { encoding: "utf-8" })
+    if (env === Enviroment.Production) {
+        const minifiedJS = await minify({
+            "loader.js": loaderDOMHelper,
+            "lib.js": lib
+        }, {
+            toplevel: true,
+            keep_classnames: true,
+            keep_fnames: false,
+            compress: true
+        })
+        outJS = minifiedJS.code
+    }
+
+    if (env === Enviroment.Development) {
+        outJS = `${loaderDOMHelper}\n\r${lib}`
+    }
+
+    writeFileSync(atomicreactMinifiedPath, outJS, { encoding: "utf-8" })
 
     const atomicreactMinifiedStatFile = statSync(atomicreactMinifiedPath)
 
